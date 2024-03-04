eeabf6b3f3f777093b3f9b8dc69ebd32bec965f2
tom4sdvorak <tom4s.dvorak@gmail.com>
2024-03-04 20:07:34 +0200

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import Service from './pages/Service';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -50,6 +51,9 @@ const App: React.FC = () => (
           <Route path="/tab3">
             <Tab3 />
           </Route>
+          <Route path="/service">
+            <Service />
+          </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
           </Route>
@@ -57,15 +61,15 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonLabel>About</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonLabel>Contact</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,24 +1,70 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+// pages/Tab1.tsx
+import {
+  IonApp,
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  } from "@ionic/react";
+import "./Tab1.css";
+import { homeOutline, reorderThreeOutline, apertureOutline } from "ionicons/icons";
 
 const Tab1: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+    <IonApp>
+      <IonMenu contentId="main-content">
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle>Menu</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
+        <IonContent className="ion-padding">
+          <IonList>
+            <IonListHeader>
+              <IonLabel>Navigate from here..</IonLabel>
+            </IonListHeader>
+            <IonMenuToggle autoHide>
+              <IonItem button routerLink="/tab1">
+                <IonIcon slot="start" icon={homeOutline}></IonIcon>
+                <IonLabel>Home</IonLabel>
+              </IonItem>
+              <IonItem routerLink="/service">
+                <IonIcon slot="start" icon={apertureOutline}></IonIcon>
+                <IonLabel>Service</IonLabel>
+              </IonItem>
+            </IonMenuToggle>
+          </IonList>
+        </IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuToggle>
+                <IonButton>
+                  <IonIcon slot="start" icon={reorderThreeOutline}></IonIcon>
+                </IonButton>
+              </IonMenuToggle>
+            </IonButtons>
+            <IonTitle>Header</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <h1>Main Content</h1>
+          <p>Click the icon in the top left to toggle the Menu.</p>
+        </IonContent>
+      </IonPage>
+    </IonApp>
   );
 };
 
